23e2393661eafadd39d1df890cc03b6bdf040548
wpollad <wpolladd@gmail.com>
2022-11-14 16:29:57 +0300

    change background

--- moving.js
+++ moving.js
@@ -1,41 +1,54 @@
 logo.style.position = "absolute";
+logo2.style.position = "absolute";
+
+let toPX = (x) => x.toString + "px";
 
 h = document.documentElement.clientHeight;
 w = document.documentElement.clientWidth;
 
-maxHeight = h - 0.1 * h;
-maxWidth = w - 0.1 * w;
+d = 0.1 * w;
+d2 = 10;
 
-var x = Math.round( Math.random() * maxWidth );
-var y = Math.round( Math.random() * maxHeight );
+maxHeight = h - d;
+maxWidth = w - d;
 
-var posX = x.toString() + "px";
-var posY = y.toString() + "px";
+let x = Math.round( Math.random() * maxWidth );
+let y = Math.round( Math.random() * maxHeight );
 
-logo.style.left = posX; 
-logo.style.top = posY;
+
+logo.style.left = toPX(x); 
+logo.style.top = toPX(y);
 
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-var dx = 8;
-var dy = 12;
+let dx = 8;
+let dy = 12;
 
 async function move(){
     while (true){
         await sleep(50);
-        var posX = x.toString() + "px";
-        var posY = y.toString() + "px";
+        let posX = x.toString() + "px";
+        let posY = y.toString() + "px";
         logo.style.left = posX;
         logo.style.top = posY;
+
+        let posX2 = (x + d2).toString() + "px";
+        let posY2 = (y + d2).toString() + "px";
+        logo2.style.left = posX2;
+        logo2.style.top = posY2;
+
         x = x + dx;
         y = y + dy;
         if ((x > maxWidth) || (x < 0)) dx = -dx; 
         if ((y > maxHeight) || (y < 0)) dy = -dy;
+
     }
 }
 move();
+
+
 
 
 
